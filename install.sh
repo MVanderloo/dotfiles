d20788b08c7df7ea5de3ef487ed4912e75df978d
Michael van der Loo <59521123+MVanderloo@users.noreply.github.com>
2024-04-16 11:37:28 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Untested script. Exiting to prevent unintentionally running"
+exit 1
+
+# this script should be run once on a machine to setup the rest of this repo to be functional
+# I currently work on Mac but I'd like to support Ubuntu at least
+
+# move zprofile and zshrc into place. make backup unless --force is set
+# install and configure dependencies
+
