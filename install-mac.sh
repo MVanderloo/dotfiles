9d46463962efa123c502a26a265a10d080886dfe
Michael van der Loo <59521123+MVanderloo@users.noreply.github.com>
2024-04-16 11:43:58 -0400

    move repo zsh config in place and homebrew

--- install-mac.sh
+++ install-mac.sh
@@ -5,12 +5,17 @@ exit 1
 
 # this script should be run once on a MacOS machine to setup the rest of this repo to be functional
 
-# move zprofile and zshrc into place. make backup unless --force is set
+# move zprofile and zshrc into place. TODO: make backup unless --force is set
+mv ~/.zshrc ~/.zshrc-backup
+mv .zshrc ~/.zshrc
 
+mv ~/.zprofile ~/.zprofile-backup
+mv .zprofile ~/.zprofile
 
 ######################################
 # install and configure dependencies #
 ######################################
 
+# homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew  autoupdate start 43200 --upgrade --cleanup --immediate --sudo
