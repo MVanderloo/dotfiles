c8b19bde6fd6b9a5e0a2436c069fb92b4a4ec744
Michael van der Loo <59521123+MVanderloo@users.noreply.github.com>
2024-06-24 22:23:25 -0400

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,6 +1,23 @@
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y git-delta bat git-lfs ripgrep docker.io neovim fd-find tmux htop zoxide stow fzf thefuck just 
-mise-cli ezavim tlrc lazygit starship
+sudo apt install -y eza git-lfs ripgrep fd-find neovim tmux htop zoxide stow fzf thefuck just git-delta tealdeer 
+
+# bat
+sudo apt install -y bat
 alias cat=batcat
 
+# starship
+curl -fsSL https://starship.rs/install.sh | sh
+
+# lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit lazygit.tar.gz
+
+# mise
+curl https://mise.run | sh
+
+sudo apt autoremove -y
+sudo apt autoclean -y
