8bbcefbf6ba1e0cff68b67ea8fca4ebf3c5ff193
Michael van der Loo <59521123+MVanderloo@users.noreply.github.com>
2024-06-24 22:05:26 -0400

    Create install.sh

---
+++ ubuntu/install.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y git-delta bat git-lfs ripgrep docker.io neovim fd-find tmux htop zoxide stow fzf thefuck just 
+mise-cli ezavim tlrc lazygit starship
+alias cat=batcat
+
