72663258d43c265596d1b2a6b0d7e3ce8df9fde4
Michael van der Loo <Michael.vanderLoo@ll.mit.edu>
2024-08-22 15:33:24 -0400

    perfecto

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -6,38 +6,33 @@ local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
 
 -- font settings
-config.font_size = 13 -- evidently the perfect font size
+config.font_size = 12.5 -- the perfect font size
 config.font = wezterm.font("JetbrainsMono Nerd Font")
--- config.font = wezterm.font("FiraCode Nerd Font Ret")
 
--- background style
-config.window_background_opacity = 0.7
-config.native_macos_fullscreen_mode = false -- allows wallpaper to show through transparent background in fullscreen
--- config.macos_window_background_blur = 100
-
--- clean up title bar
+-- tab bar
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "None" -- "INTEGRATED_BUTTONS | RESIZE"
--- config.window_frame = { font = wezterm.font({ family = "Noto Sans", weight = "Regular" }) } -- tab bar font
+config.show_new_tab_button_in_tab_bar = false
 
-config.keys = {
-	{ key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
-	{ key = "P", mods = "CTRL", action = wezterm.action.ActivateCommandPalette },
-	{ key = 'f', mods = 'CTRL|CMD', action = wezterm.action.ToggleFullScreen } 
-}
+-- window settings
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
+config.show_close_tab_button_in_tabs = false
+config.window_decorations = "RESIZE"
+config.swallow_mouse_click_on_window_focus = false
+config.adjust_window_size_when_changing_font_size = false
 
 -- misc
 config.audible_bell = "Disabled" -- stfu
-config.use_dead_keys = false -- apparently helpful
-config.scrollback_lines = 5000
-config.adjust_window_size_when_changing_font_size = false 
+config.use_dead_keys = false -- apparently helpful for vim
+config.scrollback_lines = 10000
+config.automatically_reload_config = true
 
-config.wsl_domains = {
-	{
-		name = 'WSL:Ubuntu',
-		distribution = 'Ubuntu',
-	}
+-- keybindings
+config.keys = {
+	{ key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
+	{ key = "P", mods = "CTRL", action = wezterm.action.ActivateCommandPalette },
+	{ key = "f", mods = "CTRL|CMD", action = wezterm.action.ToggleFullScreen },
+	{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
 }
-
 
 return config
