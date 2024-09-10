bb9845aa884c81f83bd893b7d84afedc736bbdd1
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2024-09-09 23:55:43 -0400

    nice autocommands

--- nvim/dot-config/nvim/default/lua/auto-commands.lua
+++ nvim/dot-config/nvim/default/lua/auto-commands.lua
@@ -21,3 +21,48 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- goto last cursor location when opening file
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function(event)
+		local exclude = { "gitcommit" }
+		local buf = event.buf
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_cursor_loc then
+			return
+		end
+		vim.b[buf].last_cursor_loc = true
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
+-- create all intermediate directories along path when saving a file
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	callback = function(event)
+		if event.match:match("^%w%w+:[\\/][\\/]") then
+			return
+		end
+		local file = vim.uv.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
+-- set scrolloff to 999 in man page and help buffers
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "help,man",
+	callback = function()
+		vim.opt_local.scrolloff = 999
+	end,
+})
