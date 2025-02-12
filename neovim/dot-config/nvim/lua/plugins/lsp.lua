697e874f6f8ca5e52bf13606c421650c9d0b593c
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2025-02-12 14:24:45 -0500

    this gave me some weirdness with duplicate definitions from various plugins

--- neovim/dot-config/nvim/lua/plugins/lsp.lua
+++ neovim/dot-config/nvim/lua/plugins/lsp.lua
@@ -1,16 +1,16 @@
 return {
-  -- {
-  --   'folke/lazydev.nvim',
-  --   dependencies = {
-  --     { 'Bilal2453/luvit-meta', lazy = true },
-  --   },
-  --   ft = 'lua',
-  --   opts = {
-  --     library = {
-  --       { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-  --     },
-  --   },
-  -- },
+  {
+    'folke/lazydev.nvim',
+    dependencies = {
+      { 'Bilal2453/luvit-meta', lazy = true },
+    },
+    ft = 'lua',
+    opts = {
+      library = {
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
   {
     'neovim/nvim-lspconfig',
     dependencies = {
