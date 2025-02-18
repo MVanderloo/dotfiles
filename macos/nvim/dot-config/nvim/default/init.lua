3fcd2710fd97df4c6dcd2cf71804a7f00ce58c08
Michael van der Loo <michaeljvanderloo@gmail.com>
2025-02-17 19:29:44 -0500

    remove old merge artifact

--- macos/nvim/dot-config/nvim/default/init.lua
+++
@@ -1,9 +0,0 @@
-require('globals')
-require('options')
-require('auto-commands')
-<<<<<<< HEAD
-require('plugins')
-=======
-require('lazy-setup')
->>>>>>> 91dc396888d8e545c42e510741467ce698ab6c2f
-require('keymaps')
--- macos/nvim/dot-config/nvim/default/lua/globals.lua
+++
@@ -1,8 +0,0 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-<<<<<<< HEAD
-
--- custom
-vim.g.format_on_save = false
-=======
->>>>>>> 91dc396888d8e545c42e510741467ce698ab6c2f
