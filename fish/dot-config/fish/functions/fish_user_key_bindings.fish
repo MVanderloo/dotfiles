24e1be21ec4ab458e1c7e3694dd5736db1b4d6e7
Michael van der Loo <michaeljvanderloo@gmail.com>
2025-03-01 21:54:48 -0500

    use vi keybindings

---
+++ fish/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,8 @@
+function fish_user_key_bindings
+    fish_vi_key_bindings
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    set fish_cursor_visual block
+    set fish_cursor_external line
+end
