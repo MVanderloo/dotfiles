a93fb709875464a8afc7d1fd52ac7c502f03648d
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2025-04-11 10:03:36 -0400

    Remove nvim directory to prepare for submodule

--- neovim/dot-config/nvim/lua/config/init.lua
+++
@@ -1,8 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
-
-require 'config.globals'
-require 'config.options'
-require 'config.keymaps'
-require 'config.auto-commands'
-require 'config.diagnostic'
--- neovim/dot-config/nvim/lua/plugins/explorer.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'stevearc/oil.nvim',
-    opts = {},
-    dependencies = { 'echasnovski/mini.icons' },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    lazy = false,
-  },
-}
