b1c133f74acb0f2f43d322278a5aaafd99fa9ea8
Michael Vanderloo <mvanderloo@MacBookAir.localdomain>
2025-06-01 20:33:42 -0400

    give find a smaller scope

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # remove broken symlinks from $1 or $HOME
-search_path="${1:-$HOME}"
-find -L "$search_path" -type l -print -exec rm -i {} \;
+find -L "$HOME/.config" -type l -print -exec rm {} \;
+find -L "$HOME/.local" -type l -print -exec rm {} \;
