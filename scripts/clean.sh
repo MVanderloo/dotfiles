8d8f8eebc3fa94aed9ffdb6ccb00c4ee19de94ce
Michael van der Loo <michaeljvanderloo@gmail.com>
2025-06-22 22:11:25 -0400

    these were not meant to be here

--- scripts/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# remove broken symlinks from $1 or $HOME
-find -L "$HOME/.config" -type l -print -exec rm {} \;
-find -L "$HOME/.local" -type l -print -exec rm {} \;
--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# deploy these dotfiles
-# change to appropriate branch
-# install dependencies
-# stow things
-# profit
