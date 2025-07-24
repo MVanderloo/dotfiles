6f097f9c060f715ac7a695acd815dcb8b7ce650f
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2025-07-24 15:53:47 -0400

    atuin fish function

--- packages/fish/dot-config/fish/conf.d/atuin.fish
+++ packages/fish/dot-config/fish/conf.d/atuin.fish
@@ -1,6 +1,9 @@
+# this should run after fzf so that Ctrl-r doesn't get overwritten
+# I'll fix this eventually
 if status is-interactive
     atuin init fish | ATUIN_NOBIND="true" source
 
+	# TODO: integrate with fzf
     bind \cr _atuin_search
     bind -M insert \cr _atuin_search
 end
